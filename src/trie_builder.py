af1f04811fe9c001053fa5c372298bc25b6c68e8
daniele <zeolla.d@gmail.com>
2024-03-02 21:04:53 +0100

    Rimosso print per velocizzare trie builder

--- src/trie_builder.py
+++ src/trie_builder.py
@@ -14,7 +14,7 @@ def build_trie(input_file_path: str, trie_file_dump:str):
     with gzip.open(input_file_path, "rb") as file:
 
         for index,transaction in enumerate(ijson.items(file, 'item.transactions.item')):
-            print(f'Parsing transaction {index}\r', end=' ')
+            #print(f'Parsing transaction {index}\r', end=' ')
             # If the destination address is None then it is a smart contract creation
             from_address = transaction.get('from').get('address')
             to_address = transaction.get('to').get('address')
